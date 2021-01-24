3baaaadb5d1770123d9c8e10cfe128fa6d402109
KimYoungJoo <kyle2123@naver.com>
2021-01-24 22:08:49 +0900

    1926. 간단한 369 게임 풀이

---
+++ SWEA/D2/[1926]간단한369게임.py
@@ -0,0 +1,12 @@
+# 1926. 간단한 369 게임
+
+n = int(input())
+for i in range(1, n + 1):
+    digits = str(i)
+    if ('3' in digits) or ('6' in digits) or ('9' in digits):
+        count = digits.count('3') + digits.count('6') + digits.count('9')
+        print('-' * count, end=' ')
+    else:
+        print(i, end=' ')
+
+print([1, 2] in [2, 3, 4])
