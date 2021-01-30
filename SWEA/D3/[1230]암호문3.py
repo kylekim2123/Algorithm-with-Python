1c4d979a5bf1d927fb1fc9ca146e55b1df8fd33e
KimYoungJoo <kyle2123@naver.com>
2021-01-31 02:35:51 +0900

    1230. 암호문3 풀이

---
+++ SWEA/D3/[1230]암호문3.py
@@ -0,0 +1,26 @@
+# 1230. S/W 문제해결 기본 8일차 - 암호문3
+
+for t in range(1, 11):
+    raw_data_len = int(input())
+    raw_data = list(input().split())
+    order_len = int(input())
+    order_data = list(input().split())
+
+    while order_data:
+        order = order_data.pop(0)
+        if order == 'A':
+            y = int(order_data.pop(0))
+            for _ in range(y):
+                raw_data.append(order_data.pop(0))
+            continue
+        x = int(order_data.pop(0))
+        y = int(order_data.pop(0))
+        if order == 'I':
+            for _ in range(y):
+                raw_data.insert(x, order_data.pop(0))
+                x += 1
+        elif order == 'D':
+            for _ in range(y):
+                raw_data.pop(x)
+    print(f'#{t}', end=' ')
+    print(*raw_data[0:10])
