a0e8c3321ce0d8a27c2db11db27782090df1a6e1
KimYoungJoo <kyle2123@naver.com>
2021-02-04 21:50:16 +0900

    10912. 외로운 문자 풀이

---
+++ SWEA/D3/[10912]외로운문자.py
@@ -0,0 +1,17 @@
+# 10912. 외로운 문자
+
+for t in range(1, int(input())+1):
+    sentence = input()
+    result = ''
+    for digit in sentence:
+        count = sentence.count(digit)
+        if (count == 1) or ((count % 2) and (digit not in result)):
+            result += digit
+    
+    if result:
+        sorted_result = ''.join(sorted(list(result)))
+        print(f'#{t} {sorted_result}')
+    else:
+        print(f'#{t} Good')
+    
+            
