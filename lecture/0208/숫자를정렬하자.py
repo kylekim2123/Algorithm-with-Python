5c129ccf7f2ff576bf3219b625507a3fb4ca611e
KimYoungJoo <kyle2123@naver.com>
2021-02-08 16:30:14 +0900

    숫자를 정렬하자 풀이

---
+++ lecture/0208/숫자를정렬하자.py
@@ -0,0 +1,13 @@
+
+T = int(input())
+for t in range(1, T+1):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    for i in range(len(a)-1, 0, -1):
+        for j in range(i):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
+    print('#%s' % t, end=' ')
+    print(*a)
