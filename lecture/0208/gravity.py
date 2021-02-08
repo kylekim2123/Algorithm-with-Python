f2dfbcff53dd5c29c8415d6281034e6b1b60f036
KimYoungJoo <kyle2123@naver.com>
2021-02-08 16:24:13 +0900

    gravity 풀이

--- lecture/0208/gravity.py
+++ lecture/0208/gravity.py
@@ -1,8 +1,24 @@
 import sys
 
-sys.stdin = open("input.txt", "r")
+sys.stdin = open("gravityInput.txt", "r")
 
 T = int(input())
 for t in range(1, T+1):
     n = int(input())
     boxes = list(map(int, input().split()))
+
+    length = len(boxes)
+    drops = [0] * length
+    for i in range(length):
+        count = 0
+        for j in range(length):
+            if boxes[i] <= boxes[j]:
+                count += 1
+        drops[i] = length - i - count
+
+    max_drop = 0
+    for drop in drops:
+        if drop > max_drop:
+            max_drop = drop
+
+    print('#%s %s' % (t, max_drop))
