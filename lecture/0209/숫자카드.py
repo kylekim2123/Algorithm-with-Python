0eaccc99273cffe223bd0b4adb4d4f4e20a49421
KimYoungJoo <kyle2123@naver.com>
2021-02-09 14:36:57 +0900

    숫자카드 풀이

---
+++ lecture/0209/숫자카드.py
@@ -0,0 +1,14 @@
+T = int(input())
+for t in range(1, T+1):
+    n = int(input())
+    cards = input()
+
+    check = [0] * 10
+    for card in cards:
+        check[int(card)] += 1
+
+    card_count, card_number = 0, 0
+    for i in range(10):
+        if check[i] >= card_count:
+            card_number, card_count  = i, check[i]
+    print('#%s %s %s' % (t, card_number, card_count))
