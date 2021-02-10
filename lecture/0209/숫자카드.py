9c377250eab7d909baed0b05bc0d48bf73226ad8
KimYoungJoo <kyle2123@naver.com>
2021-02-10 12:45:35 +0900

    두 개의 숫자열 풀이

--- lecture/0209/숫자카드.py
+++ lecture/0209/숫자카드.py
@@ -10,5 +10,5 @@ for t in range(1, T+1):
     card_count, card_number = 0, 0
     for i in range(10):
         if check[i] >= card_count:
-            card_number, card_count  = i, check[i]
+            card_number, card_count = i, check[i]
     print('#%s %s %s' % (t, card_number, card_count))
---
+++ lecture/0210/두개의숫자열.py
@@ -0,0 +1,21 @@
+def max_numbers(short_list, long_list, s_length, l_length):
+    for i in range(l_length - s_length + 1): # index out of range 방지
+        sliced_long = long_list[i:i+s_length]
+        total = 0
+        for j in range(s_length):
+            total += short_list[j] * sliced_long[j]
+        if (i == 0) or (max_num < total):
+            max_num = total
+    return max_num
+
+
+for t in range(1, int(input()) + 1):
+    n, m = map(int, input().split())
+    ai = list(map(int, input().split()))
+    bj = list(map(int, input().split()))
+
+    print('#%s' % t, end=' ')
+    if n > m:
+        print(max_numbers(bj, ai, m, n))
+    else:
+        print(max_numbers(ai, bj, n, m))
