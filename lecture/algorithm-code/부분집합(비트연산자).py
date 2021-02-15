54ff0cefe14bc4c8d318d760c9b4e708ac71d80c
KimYoungJoo <kyle2123@naver.com>
2021-02-15 12:56:04 +0900

    비트연산자를 이용한 부분집합 구하는 코드 추가

---
+++ lecture/algorithm-code/부분집합(비트연산자).py
@@ -0,0 +1,11 @@
+# 비트연산자를 이용한 부분집합 출력
+
+arr = [1, 2, 3, 4, 5]
+n = len(arr)
+
+for i in range(1<<n):
+    for j in range(n+1):
+        if i & (1<<j):
+            print(arr[j], end=', ')
+    print()
+print()
