255bb93ed429cb290284b201d0e4b4c738e8e73d
KimYoungJoo <kyle2123@naver.com>
2021-02-17 20:48:47 +0900

    GNS, String 문제 주석 추가

--- lecture/0217/GNS.py
+++ lecture/0217/GNS.py
@@ -1,13 +1,14 @@
+# 딕셔너리와 카운팅을 이용한 문자열 숫자 정렬
 def counting_sort_by_string(a):
     numbers = {"ZRO": 0, "ONE": 1, "TWO": 2, "THR": 3, "FOR": 4, "FIV": 5, "SIX": 6, "SVN": 7, "EGT": 8, "NIN": 9}
-    b = [0] * 10
-    c = [''] * len(a)
+    b = [0] * 10 # 카운팅 배열
+    c = [''] * len(a) # 정렬된 배열
     for num in a:
-        b[numbers[num]] += 1
+        b[numbers[num]] += 1 # 문자열을 숫자로 변환하여 b에 해당 값 + 1
     for i in range(1, len(b)):
         b[i] += b[i-1]
     for i in range(len(a)-1, -1, -1):
-        b_idx = numbers[a[i]]
+        b_idx = numbers[a[i]] # 문자열을 숫자로 변환하여 b의 인덱스로 할당
         c[b[b_idx]-1] = a[i]
         b[b_idx] -= 1
     return c
--- lecture/0217/String.py
+++ lecture/0217/String.py
@@ -2,10 +2,11 @@ for _ in range(1, 11):
     t = int(input())
     target, sentence = input(), input()
     count = 0
+    # 문자열의 처음부터 끝(문자열 길이 - 타겟 길이 + 1)까지 반복하면서 일일이 비교
     for i in range(len(sentence)-len(target)+1):
         for j in range(len(target)):
             if sentence[i+j] != target[j]:
-                break
+                break # 하나라도 다르면 break
         else:
-            count += 1
+            count += 1 # break가 되지 않았다는 것은 일치한다는 뜻이므로 count + 1
     print('#%s %s' % (t, count))
