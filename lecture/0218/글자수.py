a97d2706de352c22bb16758a071b87ccf302f54f
KimYoungJoo <kyle2123@naver.com>
2021-02-18 16:32:13 +0900

    글자수 - 딕셔너리 + 집합 사용으로 개선

--- lecture/0218/글자수.py
+++ lecture/0218/글자수.py
@@ -1,4 +1,7 @@
 for t in range(1, int(input())+1):
-    str1, str2 = input(), input()
-    counts = [str2.count(char) for char in str1]
-    print('#%s %s' % (t, max(counts)))
+    str1, str2 = set(input()), input() # str1을 set으로 만들어서 중복 제거
+    count = dict.fromkeys(str1, 0) # str1의 문자를 key로 딕셔너리 생성. value는 0으로 초기화
+    for x in str2:
+        if x in str1:
+            count[x] += 1 # str2를 돌면서 str1에 있는 문자가 있으면 count + 1
+    print('#%s %s' % (t, max(count.values())))
