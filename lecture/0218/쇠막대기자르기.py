afb9606f19bb4343d0c66dec51bfc6c9690d0c8b
KimYoungJoo <kyle2123@naver.com>
2021-02-18 15:31:02 +0900

    주석 추가

--- lecture/0218/쇠막대기자르기.py
+++ lecture/0218/쇠막대기자르기.py
@@ -1,15 +1,16 @@
+# pop()을 사용하여 실행 시간이 긴 풀이
 for t in range(1, int(input())+1):
     line = input()
-    pieces = []
+    pieces = [] # 쇠막대기
     count = 0
     for bracket in line:
         if bracket == '(':
-            pieces.append(0)
+            pieces.append(0) # 여는 괄호일 때는 쇠막대기 추가
             continue
-        if pieces[-1] == 0:
+        if pieces[-1] == 0: # 레이저라면
             pieces.pop()
             for i in range(len(pieces)):
-                pieces[i] += 1
+                pieces[i] += 1 # 레이저가 등장하면 쇠막대기에 +1 씩 더함
             continue
-        count += pieces.pop()+1
+        count += pieces.pop()+1 # 레이저가 아니라면, +1을 더해서 count에 합함. 1번 자르면 2개의 토막이 생기기 때문
     print('#%s %s' % (t, count))
