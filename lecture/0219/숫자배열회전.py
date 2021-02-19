dd4de3900bb0f00a3145b00ff11e5770a2011fd2
KimYoungJoo <kyle2123@naver.com>
2021-02-19 14:15:02 +0900

    숫자 배열 회전 풀이

---
+++ lecture/0219/숫자배열회전.py
@@ -0,0 +1,17 @@
+def turn(n, arr):
+    # j는 row, i는 column이다. row는 역방향, column은 정방향으로 읽어 배열을 90도 회전한다.
+    new_arr = [''.join([arr[j][i] for j in range(n-1, -1, -1)]) for i in range(n)]
+    return new_arr
+
+
+for t in range(1, int(input()) + 1):
+    n = int(input())
+    turn1 = turn(n, [input().split() for _ in range(n)])
+    turn2 = turn(n, turn1)
+    turn3 = turn(n, turn2)
+
+    print('#%s' % t)
+    for i in range(n):
+        print(turn1[i], end=' ')
+        print(turn2[i], end=' ')
+        print(turn3[i])
