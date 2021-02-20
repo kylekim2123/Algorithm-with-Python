02c0ffd74f495f4488f58d0a9d267bfdceac7146
KimYoungJoo <kyle2123@naver.com>
2021-02-20 17:28:31 +0900

    11315. 오목 판정 풀이

---
+++ SWEA/D3/[11315]오목판정.py
@@ -0,0 +1,25 @@
+# 11315. 오목 판정
+
+# 좌하, 하, 우하, 우
+dr = [1, 1, 1, 0]
+dc = [-1, 0, 1, 1]
+
+def is_five(board, n):
+    for r in range(n):
+        for c in range(n):
+            for i in range(4):
+                temp_r, temp_c = r, c
+                for _ in range(5):
+                    if temp_r < 0 or temp_r >= n or temp_c < 0 or temp_c >= n or board[temp_r][temp_c] != 'o':
+                        break
+                    temp_r += dr[i]
+                    temp_c += dc[i]
+                else:
+                    return True
+    return False
+
+for t in range(1, int(input())+1):
+    n = int(input())
+    board = [input() for _ in range(n)]
+    result = 'YES' if is_five(board, n) else 'NO'
+    print(f'#{t} {result}')
