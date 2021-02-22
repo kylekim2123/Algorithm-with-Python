f701bdddaece7290f4e2aa337dab5b07b921fccb
KimYoungJoo <kyle2123@naver.com>
2021-02-22 17:51:45 +0900

    괄호 검사 주석 추가

--- lecture/0222/괄호검사-리스트.py
+++ lecture/0222/괄호검사-리스트.py
@@ -12,15 +12,15 @@ for t in range(1, int(input())+1):
     stack = []
     for unit in sentence:
         if unit == '(' or unit == '{':
-            push(unit)
+            push(unit) # 열린 괄호면 스택에 삽입
         elif unit == ')' and pop() != '(':
-            print('#%s 0' % t)
+            print('#%s 0' % t) # 닫힌 괄호가 나올때, 스택의 마지막 요소가 짝이 맞는 열린 괄호가 아니면, 0을 출력한다.
             break
         elif unit == '}' and pop() != '{':
-            print('#%s 0' % t)
+            print('#%s 0' % t) # 닫힌 괄호가 나올때, 스택의 마지막 요소가 짝이 맞는 열린 괄호가 아니면, 0을 출력한다.
             break
     else:
         if stack:
-            print('#%s 0' % t)
+            print('#%s 0' % t) # input의 끝까지 돌았는데, 괄호가 남아있다면 짝이 맞지 않다는 것이므로 0을 출력한다.
         else:
             print('#%s 1' % t)
