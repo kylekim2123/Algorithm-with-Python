4a313c30420f76c647b03389c0389758a8531dac
KimYoungJoo <kyle2123@naver.com>
2021-02-25 02:06:18 +0900

    3314. 보충학습과 평균 풀이

--- SWEA/D3/[3314]보충학습과평균.py
+++ SWEA/D3/[3314]보충학습과평균.py
@@ -1,6 +1,5 @@
 # 3314. 보충학습과 평균
 
 for t in range(1, int(input())+1):
-    scores = list(map(lambda x: max(40, int(x)), input().split()))
-    print(f'#{t} {sum(scores)//5}')
+    print(f'#{t} {sum(list(map(lambda x: max(40, int(x)), input().split())))//5}')
     
