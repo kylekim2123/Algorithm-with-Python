d50182b368d451f2606da1763015c0564ea0eae5
KimYoungJoo <kyle2123@naver.com>
2021-02-26 11:30:39 +0900

    3809. 화섭이의 정수 나열 풀이

---
+++ SWEA/D3/[3809]화섭이의정수나열.py
@@ -0,0 +1,20 @@
+# 3809. 화섭이의 정수 나열
+
+for t in range(1, int(input())+1):
+    n = int(input())
+    numbers = list(map(int, input().split()))
+    while len(numbers) != n:
+        numbers.extend(list(map(int, input().split())))
+    i, j = 10, 1
+    while True:
+        temp1 = set(range(i//10, i))
+        temp2 = set()
+        for k in range(0, n-j+1):
+            temp2.add(int(''.join(map(str, numbers[k:k+j]))))
+        temp3 = temp1 - temp2
+        if not temp3:
+            i *= 10
+            j += 1
+        else:
+            print(f'#{t} {min(temp3)}')
+            break
