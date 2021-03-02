c8b14f467629a4dbba6949fa29b2028e69c6dbe3
KimYoungJoo <kyle2123@naver.com>
2021-03-02 11:00:19 +0900

    미로, Forth 주석 추가

--- lecture/0302/Forth.py
+++ lecture/0302/Forth.py
@@ -8,23 +8,23 @@ def forth():
     stack = []
     for unit in postfix:
         if unit.isdigit():
-            stack.append(int(unit))
+            stack.append(int(unit)) # 숫자면 스택에 push
             continue
         if unit in operators:
             if len(stack) < 2:
-                return 'error'
+                return 'error' # 에러1. 연산자가 너무 많이 입력된 경우
             b, a = stack.pop(), stack.pop()
-            stack.append(calculator[unit](a, b))
+            stack.append(calculator[unit](a, b)) # 딕셔너리의 key로 접근하여 lambda를 통해 계산
             continue
         if unit == '.':
             if len(stack) > 1:
-                return 'error'
+                return 'error' # 에러2. 마침표가 들어왔는데, 스택에 2개 이상 남아있는 경우
             return stack[0]
-        return 'error'
+        return 'error' # 에러3. "피연산자, 연산자, 마침표" 이외의 값이 입력된 경우
 
 for t in range(1, int(input())+1):
     postfix = input().split()
     if (postfix[-1] != '.') or ('.' in postfix[:-1]):
-        print('#%s error' % t)
+        print('#%s error' % t) # 에러4. 마지막이 마침표로 끝나지 않거나, 마침표가 앞쪽에서 입력된 경우
         continue
     print('#%s %s' % (t, forth()))
--- lecture/0302/미로.py
+++ lecture/0302/미로.py
@@ -1,27 +1,28 @@
+# 상 하 좌 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def dfs(x, y):
-    for i in range(4):
+    for i in range(4): # 4방향에 대하여
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and maze[nx][ny] == '0':
-            maze[nx][ny] = '1'
-            dfs(nx, ny)
+        if 0 <= nx < n and 0 <= ny < n and maze[nx][ny] == '0': # 미로의 범위를 벗어나지 않고, 통로라면
+            maze[nx][ny] = '1' # 방문 표시
+            dfs(nx, ny) # 이동
 
 for t in range(1, int(input())+1):
     n = int(input())
     maze = []
     for i in range(n):
-        line = list(input())
+        line = list(input()) # 미로의 한 줄 입력 받기
         maze.append(line)
         for j in range(n):
-            if line[j] == '2':
+            if line[j] == '2': # 시작점
                 start_x, start_y = i, j
                 break
-            if line[j] == '3':
+            if line[j] == '3': # 도착점
                 end_x, end_y = i, j
-                maze[i][j] = '0'
+                maze[i][j] = '0' # 도착할 수 없다면 '0'으로 유지되고 / 도착하면 '1'로 바뀐다. -> 그 결과를 그대로 출력
                 break
     dfs(start_x, start_y)
     print('#%s %s' % (t, maze[end_x][end_y]))
