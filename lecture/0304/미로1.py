3833557b5cb147c71abf02a4fb8beebef59a9ba4
KimYoungJoo <kyle2123@naver.com>
2021-03-04 23:26:20 +0900

    행렬찾기, 미로1 주석 추가

--- lecture/0304/미로1.py
+++ lecture/0304/미로1.py
@@ -1,6 +1,7 @@
 # 상 하 좌 우
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
+# 시작 지점 찾기
 def find_start_point():
     for i in range(16):
         for j in range(16):
@@ -16,13 +17,13 @@ def bfs(x, y):
             nx, ny = x+dx[i], y+dy[i]
             if maze[nx][ny] != '1':
                 if maze[nx][ny] == '3':
-                    return 1
+                    return 1 # 도착점에 도착하면 1 반환
                 queue.append((nx, ny))
                 maze[nx][ny] = '1'
-    return 0
+    return 0 # 도착점에 도착하지 못하면 0 반환
 
 for _ in range(1, 11):
     t = input()
     maze = [list(input()) for _ in range(16)]
-    start_x, start_y = find_start_point()
+    start_x, start_y = find_start_point() # 시작 지점
     print('#%s %s' % (t, bfs(start_x, start_y)))
--- lecture/0304/행렬찾기.py
+++ lecture/0304/행렬찾기.py
@@ -12,7 +12,7 @@ def bfs(s, e):
             if 0 <= nx < n and 0 <= ny < n and warehouse[nx][ny]:
                 queue.append((nx, ny))
                 warehouse[nx][ny] = 0
-    return x-s+1, y-e+1
+    return x-s+1, y-e+1 # x, y는 행렬의 가장 오른쪽 아래 좌표. s, e는 행렬의 가장 왼쪽 위 좌표.
 
 for t in range(1, int(input())+1):
     n = int(input())
@@ -20,11 +20,11 @@ for t in range(1, int(input())+1):
     matrix = []
     for i in range(n):
         for j in range(n):
-            if warehouse[i][j]:
+            if warehouse[i][j]: # visited를 쓰지 않고 바로 warehouse 배열에서 방문 처리를 할 수 있음
                 matrix.append(bfs(i, j))
 
-    matrix.sort(key=lambda x: (x[0]*x[1], x[0]))
-    result = [t, len(matrix)]
+    matrix.sort(key=lambda x: (x[0]*x[1], x[0])) # 행렬의 크기를 우선으로 정렬하고, 그 값이 같으면 행의 크기로 정렬
+    result = [t, len(matrix)] # 한꺼번에 출력하기 위해 테스트케이스와 행렬의 갯수를 먼저 넣음
     for unit in matrix:
         result.append(unit[0])
         result.append(unit[1])
