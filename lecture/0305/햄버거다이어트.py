94f7387029f323a0f14795847e9d86c69d50170a
KimYoungJoo <kyle2123@naver.com>
2021-03-05 15:04:34 +0900

    햄버거 다이어트 개선 풀이

--- lecture/0305/햄버거다이어트.py
+++ lecture/0305/햄버거다이어트.py
@@ -1,10 +1,12 @@
-def dfs(start, count, point, cal):
-    if count > N or cal > L:
-        return
+def make_burger(turn, point, cal):
     global max_point
-    max_point = max(max_point, point)
-    for i in range(start, N):
-        dfs(i+1, count+1, point+scores[i], cal+calories[i])
+    if cal > L or (point + future[N-turn-1]) <= max_point:
+        return
+    if turn >= N:
+        max_point = max(max_point, point)
+        return
+    make_burger(turn+1, point, cal)
+    make_burger(turn+1, point+scores[turn], cal+calories[turn])
 
 for t in range(1, int(input())+1):
     N, L = map(int, input().split())
@@ -13,6 +15,9 @@ for t in range(1, int(input())+1):
         score, calorie = map(int, input().split())
         scores.append(score)
         calories.append(calorie)
+    future = [scores[-1]]
+    for i in range(N-2, -1, -1):
+        future.append(future[-1] + scores[i])
     max_point = 0
-    dfs(0, 0, 0, 0)
+    make_burger(0, 0, 0)
     print('#%s %s' % (t, max_point))
