1eaec5327d5b537c98ccdda89574b0f0bae5422f
KimYoungJoo <kyle2123@naver.com>
2021-04-08 11:20:26 +0900

    5986. 새샘이와 세 소수 풀이

--- SWEA/D3/[5986]새샘이와세소수.py
+++ SWEA/D3/[5986]새샘이와세소수.py
@@ -1,22 +1,29 @@
 # 5986. 새샘이와 세 소수
 
-from itertools import combinations_with_replacement
-
+temp = []
 primes = [0, 0] + list(range(2, 1000))
 for i in range(2, 1000):
     if primes[i]:
         for j in range(i+i, 1000, i):
             primes[j] = 0
+        temp.append(primes[i])
 
-result = []
+
+len_temp = len(temp)
 for t in range(1, int(input())+1):
     n = int(input())
-    temp, count = [], 0
-    for prime in primes:
-        if prime and prime < n:
-            temp.append(prime)
-    for case in combinations_with_replacement(temp, 3):
-        if sum(case) == n:
-            count += 1
-    result.append(f'#{t} {count}')
-print('\n'.join(result))
+    count = 0
+    for i in range(len_temp):
+        if temp[i] >= n:
+            break
+        if temp[i]:
+            for j in range(i, len_temp):
+                if temp[j] >= n:
+                    break
+                if temp[j]:
+                    for k in range(j, len_temp):
+                        if temp[k] >= n:
+                            break
+                        if temp[k] and temp[i]+temp[j]+temp[k] == n:
+                            count += 1
+    print(f'#{t} {count}')
