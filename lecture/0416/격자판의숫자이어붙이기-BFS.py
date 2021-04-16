637b4b3725c7c682ff716487d769635ff4d0ce86
KimYoungJoo <kyle2123@naver.com>
2021-04-16 15:13:48 +0900

    격자판의 숫자 이어 붙이기 - dfs 풀이

--- lecture/0416/격자판의숫자이어붙이기-BFS.py
+++ lecture/0416/격자판의숫자이어붙이기-BFS.py
@@ -5,12 +5,12 @@ def bfs(x, y):
     queue = [(x, y, 0, grid[x][y])]
     while queue:
         x, y, count, number = queue.pop(0)
-        if count >= 6:
-            result.add(number)
-            continue
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
             if 0 <= nx < 4 and 0 <= ny < 4:
+                if count >= 5:
+                    result.add(number+grid[nx][ny])
+                    continue
                 queue.append((nx, ny, count+1, number+grid[nx][ny]))
 
 for t in range(1, int(input())+1):
---
+++ lecture/0416/격자판의숫자이어붙이기-DFS.py
@@ -0,0 +1,19 @@
+# 2. DFS
+dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+
+def dfs(x, y, count, number):
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            if count >= 5:
+                result.add(number+grid[nx][ny])
+                continue
+            dfs(nx, ny, count+1, number+grid[nx][ny])
+
+for t in range(1, int(input())+1):
+    grid = [input().split() for _ in range(4)]
+    result = set()
+    for i in range(4):
+        for j in range(4):
+            dfs(i, j, 0, grid[i][j])
+    print('#%s %s' % (t, len(result)))
