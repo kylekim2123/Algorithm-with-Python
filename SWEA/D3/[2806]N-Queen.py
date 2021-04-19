1e8df697e0db3317009d831b8f808564e94f38a1
KimYoungJoo <kyle2123@naver.com>
2021-04-19 17:07:47 +0900

    2806. N-Queen 성능 개선 풀이

--- SWEA/D3/[2806]N-Queen.py
+++ SWEA/D3/[2806]N-Queen.py
@@ -27,3 +27,27 @@ for t in range(1, int(input())+1):
     count = 0
     set_queen(0)
     print('#%s %s' % (t, count))
+
+
+"""
+성능 개선 풀이 (세로, 대각선을 일차원 배열로 만들어서 방문체크)
+
+def set_queen(row):
+    if row == n:
+        global count
+        count += 1
+        return
+    for col in range(n):
+        ld, rd = row-col+n-1, row+col
+        if not columns[col] and not left_diagonal[ld] and not right_diagonal[rd]:
+            columns[col], left_diagonal[ld], right_diagonal[rd] = 1, 1, 1
+            set_queen(row+1)
+            columns[col], left_diagonal[ld], right_diagonal[rd] = 0, 0, 0
+
+for t in range(1, int(input())+1):
+    n = int(input())
+    count = 0
+    columns, left_diagonal, right_diagonal = [0]*n, [0]*(n*2-1), [0]*(n*2-1)
+    set_queen(0)
+    print('#%s %s' % (t, count))
+"""
