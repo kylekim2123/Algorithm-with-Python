4ebccdda684f8133882495c10900181b1b9efad5
KimYoungJoo <kyle2123@naver.com>
2021-04-21 18:21:12 +0900

    0421 주석 추가

--- lecture/0421/그래프에Dijkstra적용.py
+++ lecture/0421/그래프에Dijkstra적용.py
@@ -1,5 +1,6 @@
 INF = 987654321
 
+# D배열에서 최단 경로의 노드 찾기
 def shortest_node():
     index, min_value = 0, INF
     for i, value in enumerate(D):
@@ -8,17 +9,21 @@ def shortest_node():
     return index
 
 
+# 다익스트라
 def find_shortest(start):
+    # 시작 포인트 초기화
     U[start] = True
     D[start] = 0
     for next_node, weight in graph[start]:
         D[next_node] = weight
-    for _ in range(n-2):
-        now = shortest_node()
-        U[now] = True
-        for next_node, weight in graph[now]:
-            if not U[next_node]:
-                D[next_node] = min(D[next_node], D[now] + weight)
+    
+    # 시작노드를 제외한 나머지 노드까지의 최단 경로 탐색
+    for _ in range(n-2): # n-2인 이유는, 마지막 노드도 탐색하지 않아도 되기 때문
+        now = shortest_node() # 인접 노드 중 최단 거리에 있는 노드
+        U[now] = True # 방문 처리
+        for next_node, weight in graph[now]: # 해당 노드에서 갈 수 있는 인접 노드를 대상
+            if not U[next_node]: # 인접 노드 아직 방문 안했다면
+                D[next_node] = min(D[next_node], D[now] + weight) # 인접 노드의 현재 최단거리 vs 새로 갔을 때 거리
 
 
 for t in range(1, int(input())+1):
@@ -28,7 +33,7 @@ for t in range(1, int(input())+1):
         s, e, w = input().split()
         graph[ord(s)-97].append((ord(e)-97, int(w)))
 
-    U = [False] * n
-    D = [INF] * n
+    U = [False] * n # 방문처리 배열
+    D = [INF] * n # 최단경로 배열
     find_shortest(0)
     print('#%s %s' % (t, ' '.join(map(str, D))))
--- lecture/0421/최장경로.py
+++ lecture/0421/최장경로.py
@@ -1,11 +1,11 @@
 def get_max_distance(start):
     global max_distance
-    max_distance = max(max_distance, visited[start])
-    for next_node in graph[start]:
-        if not visited[next_node]:
-            visited[next_node] = visited[start] + 1
-            get_max_distance(next_node)
-            visited[next_node] = 0
+    max_distance = max(max_distance, visited[start]) # 현재까지의 경로 수를 비교하여, 최장 경로 갱신
+    for next_node in graph[start]: # 인접 노드에 대해
+        if not visited[next_node]: # 방문하지 않았다면
+            visited[next_node] = visited[start] + 1 # 한 칸 이동 (현재 경로 + 1)
+            get_max_distance(next_node) # 그 경로를 따라서 최장 경로 탐색
+            visited[next_node] = 0 # 이동 취소
 
 
 for t in range(1, int(input())+1):
@@ -16,8 +16,8 @@ for t in range(1, int(input())+1):
         graph[x].append(y)
         graph[y].append(x)
 
-    max_distance = 0
-    for i in range(1, n+1):
+    max_distance = 0 # 최장 경로
+    for i in range(1, n+1): # 모든 노드를 탐색
         visited = [0] * (n+1)
         visited[i] = 1
         get_max_distance(i)
