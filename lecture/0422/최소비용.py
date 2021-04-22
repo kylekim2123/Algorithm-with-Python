252ddce362d55929f60c14f035c1666feb6b7339
KimYoungJoo <kyle2123@naver.com>
2021-04-22 17:44:29 +0900

    최소 비용 풀이 (다익스트라 -> BFS 로 변경)

--- lecture/0422/최소비용.py
+++ lecture/0422/최소비용.py
@@ -4,20 +4,18 @@ dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 def dijkstra(x, y):
     costs = [[INF]*n for _ in range(n)]
     costs[x][y] = 0
-    queue = [(0, x, y)]
+    queue = [(x, y)]
     while queue:
-        cost, cx, cy = queue.pop(0)
-        if costs[cx][cy] < cost:
-            continue
+        cx, cy = queue.pop(0)
         for i in range(4):
             nx, ny = cx+dx[i], cy+dy[i]
             if 0 <= nx < n and 0 <= ny < n:
-                next_cost = cost + 1
+                next_cost = costs[cx][cy] + 1
                 if H[nx][ny] > H[cx][cy]:
                     next_cost += H[nx][ny] - H[cx][cy]
                 if next_cost < costs[nx][ny]:
                     costs[nx][ny] = next_cost
-                    queue.append((next_cost, nx, ny))
+                    queue.append((nx, ny))
     return costs[n-1][n-1]
 
 
