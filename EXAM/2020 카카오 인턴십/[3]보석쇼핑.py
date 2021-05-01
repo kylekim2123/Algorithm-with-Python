68d9b68b7494885f963f4e60a0ac067eaf4384c2
KimYoungJoo <kyle2123@naver.com>
2021-05-02 00:33:22 +0900

    2020 카카오 인턴십 3번 보석 쇼핑 효율성 개선

--- EXAM/2020 카카오 인턴십/[3]보석쇼핑.py
+++ EXAM/2020 카카오 인턴십/[3]보석쇼핑.py
@@ -1,21 +1,29 @@
 def solution(gems):
     count = len(set(gems))
-    gems_type = {}
+    length = len(gems)
+    gems_type = {gems[0]: 1}
     start, end = 0, 0
-    answer = [1, len(gems)]
+    answer = [0, length-1]
 
-    while start <= end < len(gems):
-        if len(gems_type) == count and all(gems_type.values()):
-            gems_type[gems[start]] -= 1
-            start += 1
+    while end < length:
+        if len(gems_type) == count:
             if answer[1]-answer[0] > end-start:
                 answer[0], answer[1] = start, end
+            if gems_type[gems[start]] == 1:
+                del gems_type[gems[start]]
+            else:
+                gems_type[gems[start]] -= 1
+            start += 1
         else:
+            end += 1
+            if end >= length:
+                break
             gems_type.setdefault(gems[end], 0)
             gems_type[gems[end]] += 1
-            end += 1
 
+    answer[0] += 1
+    answer[1] += 1
     return answer
         
-gems = 	["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
+gems = ['DIA', 'EM', 'EM', 'RUB', 'DIA']
 print(solution(gems))
