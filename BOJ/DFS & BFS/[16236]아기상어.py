5768b0bd5395993a6613cc708ad9e995f7ed8e6a
KimYoungJoo <kyle2123@naver.com>
2021-06-10 15:07:42 +0900

    16236. 아기상어(골드4) 개선 풀이 1136ms -> 228ms

--- BOJ/DFS & BFS/[16236]아기상어.py
+++ BOJ/DFS & BFS/[16236]아기상어.py
@@ -2,49 +2,43 @@
 
 import sys
 from collections import deque
+from heapq import heappush
 input = sys.stdin.readline
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-def bfs(x, y, *end):
+def bfs(x, y):
     visited = [[0]*n for _ in range(n)]
     visited[x][y] = 1
     queue = deque([(x, y)])
+    possible_fish = []
     while queue:
         x, y = queue.popleft()
         for k in range(4):
             nx, ny = x+dx[k], y+dy[k]
             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and baby_size >= area[nx][ny]:
                 visited[nx][ny] = visited[x][y] + 1
-                if (nx, ny) == end:
-                    return visited[nx][ny] - 1
                 queue.append((nx, ny))
+                if 0 < area[nx][ny] < baby_size:
+                    heappush(possible_fish, (visited[nx][ny]-1, nx, ny))
+    return possible_fish
 
 n = int(input())
-area, fishes = [], []
+area = []
 for i in range(n):
     line = list(map(int, input().split()))
     area.append(line)
     for j in range(n):
         if line[j] == 9:
-            bx, by = (i, j)
-        elif line[j]:
-            fishes.append((i, j))
+            bx, by = i, j
 
 baby_size, baby_eating, seconds = 2, 0, 0
 while True:
     area[bx][by] = 0
-    targets = []
-    for i, j in fishes:
-        if area[i][j] < baby_size:
-            distance = bfs(bx, by, i, j)
-            if distance:
-                targets.append((distance, i , j))
+    targets = bfs(bx, by)
     if not targets:
         break
-    targets.sort(key=lambda x: (x[0], x[1], x[2]))
-    seconds += targets[0][0]
-    bx, by = targets[0][1], targets[0][2]
-    fishes.pop(fishes.index((bx, by)))
+    second, bx, by = targets[0]
+    seconds += second
     baby_eating += 1
     if baby_eating >= baby_size:
         baby_size += 1
