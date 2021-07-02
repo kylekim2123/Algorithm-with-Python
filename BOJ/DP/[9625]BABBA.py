e14a24b01e5590be10026ef015cf3bd0228a0670
KimYoungJoo <kyle2123@naver.com>
2021-07-02 11:15:05 +0900

    9625. BABBA (브론즈1) DP 풀이

--- BOJ/DP/[9625]BABBA.py
+++ BOJ/DP/[9625]BABBA.py
@@ -1,8 +1,19 @@
 # 9625. BABBA (브론즈1)
 
+# DP
+k = int(input())
+memo = [0, 1]
+for i in range(1, k):
+    memo.append(memo[i-1] + memo[i])
+print(memo[-2], memo[-1])
+
+
+"""
+# 수학
 k = int(input())
 A, B = 1, 0
 for _ in range(k):
     A, B = B, A
     B += A
 print(A, B)
+"""
