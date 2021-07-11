1da3c378fedd0fa8ba86a4b8853d2862fade4390
KimYoungJoo <kyle2123@naver.com>
2021-07-12 00:11:32 +0900

    프로그래머스 Level 3 : 징검다리 건너기

--- Programmers/Level3/징검다리건너기.py
+++ Programmers/Level3/징검다리건너기.py
@@ -1,8 +1,20 @@
 # 2019 카카오 개발자 겨울 인턴십 : 징검다리 건너기
 
 def solution(stones, k):
-    answer = 200000000
-    for i in range(len(stones)-k+1):
-        k_stones = stones[i:i+k]
-        answer = min(answer, max(k_stones))
-    return answer
+    left, right = 1, 200000000
+
+    while left < right:
+        center = (left+right) // 2
+        no_pass = 0
+        for stone in stones:
+            if stone-center <= 0:
+                no_pass += 1
+            else:
+                no_pass = 0
+            if no_pass >= k:
+                right = center
+                break
+        else:
+            left = center + 1
+
+    return left
