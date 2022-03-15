f8ac56f1e6a5c674a9e07603293cf77006ab84df
kyle <kyle2123@naver.com>
2022-03-15 14:23:10 +0900

    1339. 단어수학 (골드4)

--- BOJ/Greedy/[1339]단어수학.py
+++ BOJ/Greedy/[1339]단어수학.py
@@ -1,17 +1,18 @@
-# 1339. 단어 수학 (골드4)
+# 1339. 단어수학 (골드4)
 
 n = int(input())
-alphabet = {}
+alphabets = {}
+
 for _ in range(n):
     word = input()
-    x = len(word)-1
-    for char in word:
-        alphabet[char] = alphabet.get(char, 0) + (10**x)
-        x -= 1
+    digit = len(word) - 1
+    for i in word:
+        alphabets[i] = alphabets.get(i, 0) + (10 ** digit)
+        digit -= 1
 
-result = sorted(alphabet.items(), key=lambda x: x[1], reverse=True)
-total, num = 0, 9
-for r in result:
-    total += (r[1]*num)
-    num -= 1
+number, total = 9, 0
+for value in sorted(alphabets.values(), reverse=True):
+    total += value * number
+    number -= 1
+
 print(total)
