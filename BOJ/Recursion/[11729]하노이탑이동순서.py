237ff90226cd8d4ee19a909f2f808665f7f16bfa
kyle <kyle2123@naver.com>
2022-04-12 16:05:03 +0900

    11729. 하노이탑이동순서(실버2)

--- BOJ/Recursion/[11729]하노이탑이동순서.py
+++ BOJ/Recursion/[11729]하노이탑이동순서.py
@@ -1,17 +1,16 @@
 # 11729. 하노이 탑 이동 순서 (실버2)
 
-# 가장 밑의 원판을 제외한 n-1개의 원판을 일단 가운데로 옮기고
-# 가장 밑의 원판을 맨 오른쪽에 옮긴다음
-# n-1개의 원판을 맨 오른쪽에 옮긴다고 생각하며 재귀로 푼다.
 
-def hanoi(number, start, middle, end):
+def hanoi(start, middle, end, number):
     if number == 1:
-        print(start, end) # 원판이 하나 남은 경우
+        print(start, end)
         return
-    hanoi(number-1, start, end, middle)
-    print(start, end)
-    hanoi(number-1, middle, start, end)
+
+    hanoi(start, end, middle, number - 1)  # 가장 밑의 원판을 제외한 n-1개의 원판을 일단 가운데로 옮기고
+    print(start, end)  # 가장 밑의 원판을 맨 오른 쪽에 옮긴 다음
+    hanoi(middle, start, end, number - 1)  # n-1개의 원판을 맨 오른 쪽에 옮긴 다고 생각 하며 재귀로 푼다.
+
 
 n = int(input())
-print((2**n)-1) # 하노이 탑 최소 이동 횟수 공식
-hanoi(n, 1, 2, 3)
+print((2 ** n) - 1)  # 하노이 탑 이동 횟수 공식
+hanoi(1, 2, 3, n)
