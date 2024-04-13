1156dcc185877797cd87b717a04682a1b2385f1d
kylekim2123 <kyle2123@naver.com>
2024-04-13 12:26:08 +0900

    [BOJ] 골드5 회문

--- BOJ/Greedy/[17609]회문.py
+++ BOJ/Greedy/[17609]회문.py
@@ -1,29 +1,41 @@
-# 17609. 회문 (실버1)
-def another_loop(word, i, j):
-    while i <= len(word)//2:
-        if word[i] != word[j]:
+# 골드 5
+
+import sys
+
+input = sys.stdin.readline
+
+PALINDROME = 0
+PSEUDO_PALINDROME = 1
+NORMAL_STRING = 2
+
+
+def is_palindrome(start, end):
+    while start < end:
+        if word[start] != word[end]:
             return False
-        i += 1
-        j -= 1
+
+        start += 1
+        end -= 1
+
     return True
 
-for _ in range(int(input())):
-    word = list(input())
-    length = len(word)
-    i = 0
-    j = length - 1
-    count = 1
-    while i < length//2:
-        if word[i] == word[j]:
-            i += 1
-            j -= 1
-            continue
-        if another_loop(word, i, j-1) or another_loop(word, i+1, j):
-            print(1)
-            break
-        else:
-            print(2)
-            break
-    else:
-        print(0)
 
+def check(start, end):
+    while start < end:
+        if word[start] == word[end]:
+            start += 1
+            end -= 1
+
+            continue
+
+        if is_palindrome(start + 1, end) or is_palindrome(start, end - 1):
+            return PSEUDO_PALINDROME
+
+        return NORMAL_STRING
+
+    return PALINDROME
+
+
+for _ in range(int(input())):
+    word = input().rstrip()
+    print(check(0, len(word) - 1))
