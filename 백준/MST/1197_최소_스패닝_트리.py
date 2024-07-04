7471693eaffeb9ab98a9805649d61ee44205373f
kylekim2123 <kyle2123@naver.com>
2024-07-04 16:46:27 +0900

    [BOJ] 골드4 최소 스패닝 트리

--- 백준/MST/1197_최소_스패닝_트리.py
+++ 백준/MST/1197_최소_스패닝_트리.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 
 def find(x):
     if x != parent[x]:
-        parent[x] = find(parent[x])
+        parent[x] = find(parent[x])  # path compression
 
     return parent[x]
 
@@ -18,13 +18,14 @@ def union(x, y):
     if x_root == y_root:
         return False
 
-    if rank[x_root] < rank[y_root]:
+    # union by rank
+    if rank[x_root] > rank[y_root]:
+        parent[y_root] = x_root
+    elif rank[x_root] < rank[y_root]:
         parent[x_root] = y_root
-    elif rank[x_root] > rank[y_root]:
-        parent[y_root] = x_root
     else:
-        parent[y_root] = x_root
-        rank[x_root] += 1
+        parent[x_root] = y_root
+        rank[y_root] += 1  # 랭크가 같은 것끼리 합한 후에는 랭크 + 1을 해준다.
 
     return True
 
@@ -36,17 +37,17 @@ edges = []
 total, counts = 0, 0
 
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    edges.append((c, a, b))
+    a, b, c = map(int, input().split())  # 정점a, 정점b, 가중치c
+    edges.append((c, a, b))  # 가중치를 먼저 넣어서, 가중치 기준 정렬 되도록 함
 
-edges.sort()
+edges.sort()  # 크루스칼 알고리즘을 위한 가중치 기준 정렬
 
 for c, a, b in edges:
-    if union(a, b):
-        total += c
+    if union(a, b):  # 간선을 이으면(같은 집합에 넣으면)
+        total += c  # 가중치 더하기
         counts += 1
 
         if counts == n - 1:
-            break
+            break  # 트리의 특성 상, 간선의 개수는 (정점 - 1) 이므로 이에 도달하면 종료
 
 print(total)
