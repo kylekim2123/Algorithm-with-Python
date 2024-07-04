1cf44e28cf7c15c86465d27a2fcd7648562ab5a2
kylekim2123 <kyle2123@naver.com>
2024-07-05 03:20:28 +0900

    [BOJ] 실버1 절대값 힙

--- 백준/큐/11286_절대값_힙.py
+++ 백준/큐/11286_절대값_힙.py
@@ -11,7 +11,11 @@ heap = []
 for _ in range(n):
     x = int(input())
 
-    if x == 0:
-        print(heappop(heap)[1] if heap else 0)
+    if x != 0:
+        heappush(heap, (abs(x), x))  # x가 0이 아니라면 (절대값, 원본)을 힙에 삽입
+        continue
+
+    if heap:
+        print(heappop(heap)[1])  # x가 0이고 힙이 비어있지 않으면 원본을 출력
     else:
-        heappush(heap, (abs(x), x))
+        print(0)  # x가 0이고 힙이 비어있으면 0을 출력
