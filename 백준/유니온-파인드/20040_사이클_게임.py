d13cf775f72bd04aa78e7aa3fca54a407b9cc781
kylekim2123 <kyle2123@naver.com>
2024-07-04 15:08:52 +0900

    [BOJ] 골드4 사이클 게임

--- 백준/유니온-파인드/20040_사이클_게임.py
+++ 백준/유니온-파인드/20040_사이클_게임.py
@@ -3,26 +3,35 @@
 import sys
 
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
 
 def find(x):
     if x != parent[x]:
-        parent[x] = find(parent[x])
+        parent[x] = find(parent[x])  # path compression
+
     return parent[x]
 
 
 n, m = map(int, input().split())
 parent = list(range(n))
+rank = [0] * n
+result = 0
 
 for t in range(1, m + 1):
     x, y = map(int, input().split())
     x_root, y_root = find(x), find(y)
 
     if x_root == y_root:
-        print(t)
+        result = t
         break
 
-    parent[y_root] = x_root
-else:
-    print(0)
+    # union by rank
+    if rank[x_root] > rank[y_root]:
+        parent[y_root] = x_root
+    elif rank[x_root] < rank[y_root]:
+        parent[x_root] = y_root
+    else:
+        parent[x_root] = y_root
+        rank[y_root] += 1  # 랭크가 같은 것끼리 합한 후에는 랭크 + 1을 해준다.
+
+print(result)
