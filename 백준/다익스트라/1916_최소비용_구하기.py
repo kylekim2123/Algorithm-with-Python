10d89a48d09953c1be8f7651d123c60fcdaf1dbe
kylekim2123 <kyle2123@naver.com>
2024-07-06 02:41:52 +0900

    [BOJ] 골드5 최소비용 구하기

--- 백준/다익스트라/1916_최소비용_구하기.py
+++ 백준/다익스트라/1916_최소비용_구하기.py
@@ -7,28 +7,30 @@ input = sys.stdin.readline
 INF = 99999999
 
 
-def dijkstra(node):
-    distance = [INF] * (n + 1)
-    distance[node] = 0
-    heap = [(0, node)]
+def dijkstra(start):
+    distance[start] = 0
+    heap = [(0, start)]
 
     while heap:
         min_dist, node = heappop(heap)
 
-        if min_dist > distance[node]:
+        if distance[node] < min_dist:
             continue
 
-        for next_node, cost in graph[node]:
-            next_dist = distance[node] + cost
+        if node == end:
+            return  # 도착 도시에 대한 최단 경로가 확정 되면 더이상 탐색할 필요 없음
+
+        for next_node, dist in graph[node]:
+            next_dist = distance[node] + dist
+
             if next_dist < distance[next_node]:
                 distance[next_node] = next_dist
                 heappush(heap, (next_dist, next_node))
 
-    return distance[end]
-
 
 n, m = int(input()), int(input())
 graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)
 
 for _ in range(m):
     s, e, w = map(int, input().split())
@@ -36,4 +38,6 @@ for _ in range(m):
 
 start, end = map(int, input().split())
 
-print(dijkstra(start))
+dijkstra(start)
+
+print(distance[end])
