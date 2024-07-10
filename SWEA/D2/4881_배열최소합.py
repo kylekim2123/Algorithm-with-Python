a2992ecf678c1c691cfc7b4b84cb75c1a3446f5e
kylekim2123 <kyle2123@naver.com>
2024-07-11 00:25:20 +0900

    [SWEA] D2 배열 최소 합

--- SWEA/D2/4881_배열최소합.py
+++ SWEA/D2/4881_배열최소합.py
@@ -1,9 +1,9 @@
 # permutations
 
-def recursion(depth, total):
+def permutations(depth, total):
     global min_total
 
-    if total >= min_total:  # backtracking
+    if total >= min_total:  # 백트래킹(가지치기) -> 이미 합이 최소 이상이면 유망성이 없다고 판단
         return
 
     if depth == n:
@@ -13,15 +13,17 @@ def recursion(depth, total):
     for i in range(n):
         if not visited[i]:
             visited[i] = True
-            recursion(depth + 1, total + board[depth][i])
+            permutations(depth + 1, total + board[depth][i])
             visited[i] = False
 
 
-for t in range(1, int(input()) + 1):
+t = int(input())
+
+for test_case in range(1, t + 1):
     n = int(input())
     board = [list(map(int, input().split())) for _ in range(n)]
     visited = [False] * n
-    min_total = 90
-    recursion(0, 0)
+    min_total = 90  # N이 10까지 가능하고, 각 줄의 자연수는 10 미만이므로 최대 90까지만 가능
+    permutations(0, 0)
 
-    print(f"#{t} {min_total}")
+    print(f"#{test_case} {min_total}")
