1500789e0dca78a731e92b8efc072cab63b7eac0
kylekim2123 <kyle2123@naver.com>
2024-07-11 01:28:20 +0900

    [BOJ] 골드4 N-Queen

--- 백준/백트래킹/9663_N-Queen.py
+++ 백준/백트래킹/9663_N-Queen.py
@@ -1,26 +1,32 @@
-# 9663. N-Queen (골드5)
+# 골드 4
 
+def is_promising(x, y):
+    # 열 검사
+    if visited[y]:
+        return False
 
-# 대각선 검사
-def is_possible(x, y):
+    # 대각선 검사
     for i, j in queens:
         if abs(i - x) == abs(j - y):
             return False
+
     return True
 
 
 def set_queen(x):
-    if x == n:
+    if x == n:  # 종료조건(Base)
         global total
         total += 1
         return
 
+    # x행의 모든 i열에 대해
     for i in range(n):
-        # 열, 대각선 검사
-        if not visited[i] and is_possible(x, i):
+        if is_promising(x, i):  # 가지치기(pruning) -> 여기서 유망하지 않으면 탐색 안함
             visited[i] = True
             queens.append((x, i))
+
             set_queen(x + 1)  # 행은 겹칠 일 없음
+
             visited[i] = False
             queens.pop()
 
