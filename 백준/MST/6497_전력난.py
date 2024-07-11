20eb6925957b7eff7bc4127380b377a37d753faf
kylekim2123 <kyle2123@naver.com>
2024-07-11 17:51:59 +0900

    [BOJ] 골드4 전력난

--- 백준/MST/6497_전력난.py
+++ 백준/MST/6497_전력난.py
@@ -7,37 +7,53 @@ input = sys.stdin.readline
 
 def find(x):
     if x != parent[x]:
-        parent[x] = find(parent[x])
+        parent[x] = find(parent[x])  # path compression
+
     return parent[x]
+
+
+def union(x, y):
+    x_root, y_root = find(x), find(y)
+
+    if x_root == y_root:
+        return False
+
+    # union by rank
+    if rank[x_root] > rank[y_root]:
+        parent[y_root] = x_root
+    elif rank[x_root] < rank[y_root]:
+        parent[x_root] = y_root
+    else:
+        parent[x_root] = y_root
+        rank[y_root] += 1
+
+    return True
 
 
 while True:
     m, n = map(int, input().split())
 
     if m == n == 0:
-        break
+        break  # 0 0 이 입력되면 종료
 
     edges = []
-    total = 0
+    parent = list(range(m))
+    rank = [0] * m
+    total_cost, counts, minimum_cost = 0, 0, 0
 
     for _ in range(n):
         x, y, z = map(int, input().split())
         edges.append((z, x, y))
-        total += z
+        total_cost += z  # z를 모두 더하면 총 비용을 알 수 있음
 
     edges.sort()
-    parent = list(range(m))
-    counts, cost = 0, 0
 
     for z, x, y in edges:
-        x_root, y_root = find(x), find(y)
-
-        if x_root != y_root:
-            parent[y_root] = x_root
+        if union(x, y):
+            minimum_cost += z
             counts += 1
-            cost += z
 
             if counts >= m - 1:
-                break
+                break  # 정점이 n개면 간선이 n-1개임을 이용하여 크루스칼의 효율성 확보
 
-    print(total - cost)
+    print(total_cost - minimum_cost)  # 총 비용에서 최소 비용을 빼면 절약 비용을 알 수 있음
